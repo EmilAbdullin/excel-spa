bb9ac37951ebc3afe8f77202c77a7eb32e49c2da
Emilio1111 <lyricstyle@bk.ru>
2020-08-31 22:29:42 +0300

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ const jsLoaders = () => {
 }
 
 module.exports = {
-    context:path.resolve(__dirname, 'src'),
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: ['@babel/polyfill','./index.js'],
     output:{
