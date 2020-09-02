47756c8914074e95b2f8aece1253fe2876c46c2c
Emilio1111 <lyricstyle@bk.ru>
2020-09-02 23:42:31 +0300

    structure for dom listener and analog jquery util

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,4 +1,36 @@
 import { ExcelComponent } from '@core/ExcelComponent'
 export class Table extends ExcelComponent{
 
+    static className = 'excel__table'
+
+    toHTML() {
+        return `
+            <div class="row">
+                <div class="row-info"></div>
+                <div class="row-data">
+                    <div class="column">A</div>
+                    <div class="column">B</div>
+                    <div class="column">C</div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="row-info">1</div>
+                <div class="row-data">
+                    <div class="cell selected">A</div>
+                    <div class="cell">B</div>
+                    <div class="cell">C</div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="row-info">1</div>
+                <div class="row-data">
+                    <div class="cell selected">A</div>
+                    <div class="cell">B</div>
+                    <div class="cell">C</div>
+                </div>
+                </div>
+`
+    }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,14 @@
 import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-    toHTML(){
+    constructor($root, options = {}){
+        super($root, options.listeners)
+    }
+    toHTML() {
         return ''
+    }
+
+    init(){
+        this.initDOMListeners()
     }
 }
